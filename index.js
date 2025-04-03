352e095a4fa29b8b6483dbc9800d817513fd1f14
Victor Kinyanjui Ngigi <victor.ngigi@outlook.com>
2025-04-03 12:32:20 +0300

    final

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+function destructivelyAppendCat() {
+  cats.push("Ralph");
+  return cats;
+}
+    
+function destructivelyPrependCat() {
+  cats.unshift("Bob");
+  return cats;
+}
+function destructivelyRemoveLastCat() {
+  cats.pop();
+  return cats;
+}
+function destructivelyRemoveFirstCat() {
+  cats.shift();
+  return cats;
+}
+function appendCat() {
+  let newCats = [...cats, "Broom"];
+  return newCats;
+}
+function prependCat() {
+  let newCats = ["Arnold", ...cats];
+  return newCats;
+}
+function removeLastCat() {
+  let newCats = cats.slice(0, -1);
+  return newCats;
+}
+function removeFirstCat() {
+  let newCats = cats.slice(1);
+  return newCats;
+}
